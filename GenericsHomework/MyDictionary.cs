b309c1f4574dee724b615736c1d54925b16c4c31
ademir <ademir@N107925>
2021-01-24 12:29:29 +0300

    24012021

---
+++ GenericsHomework/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsHomework
+{
+ 
+    class MyDictionary<T>
+    {
+        T[] array;
+        T[] tempArray;
+        public MyDictionary()
+        {
+            array = new T[0];
+        }
+        public void Add(T item) 
+        {
+            tempArray = array;
+            array = new T[array.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                array[i] = tempArray[i];
+            }
+            array[array.Length - 1] = item;
+        }
+        public int Count
+        {
+            get { return array.Length; }
+        }
+        public T [] Urunler
+        {
+            get { return array; }
+        }
+    }
+    
+}
---
+++ GenericsHomework/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> urunler = new MyDictionary<string>();
+            urunler.Add("Laptop");
+            urunler.Add("Tv Ünitesi");
+            Console.Write("Toplamda " + urunler.Count + " adet ürün bulunmaktadır.\n");
+            foreach (var urun in urunler.Urunler)
+            {
+                Console.WriteLine(urun);
+            }
+            
+
+        }
+    }
+}
